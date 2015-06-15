d111e54faec759ce0ef03d94df7f970d7d53ca6d
Alexey Potopakhin <alexey_potopahin@hotmail.com>
2015-06-15 16:32:10 +0300

    Implemented list configuration operations

--- goods.js
+++ goods.js
@@ -1,13 +1,31 @@
-
+// List item identificator
 var list_id = 0;
 
 $(document).ready(function() {
-	// Input
+	// Input key Enter event
 	$("#input_goods").keydown(function(event_object) {
 		if(event_object.which == 13) {
 			addListItem($("#input_goods").val());
 			$("#input_goods").val("");
 		}
+	});
+	
+	// Strike out all items event
+	$("#strike_out_all").change(function() {
+		if($(this).is(":checked")) {
+			$(".text").attr("style", "text-decoration:line-through; opacity:0.5;");
+			$(".item_checkbox").prop("checked", true);
+		}
+		else {
+  			$(".text").attr("style", "text-decoration:none;");
+  			$(".item_checkbox").prop("checked", false);
+		}
+	});
+	
+	// Remove all items event
+	$("#remove_all_button").click(function() {
+		$(".item_checkbox:checked").parentsUntil("#goods_list").remove();
+		$("#strike_out_all").prop("checked", false);
 	});
 });
 
@@ -18,9 +36,9 @@ function addListItem(name) {
 	var table_start = "<table><tr>";
 	var table_end = "</tr></table>";
 	
-	var checkbox = "<td><input type='checkbox'></td>";
-	var text = "<td width='230'><span id='text'>" + name + "</span></td>";
-	var button = "<td><button type='button'></button></td>";
+	var checkbox = "<td><input class='item_checkbox' type='checkbox'></td>";
+	var text = "<td id='text_cell' width='230'><span class='text'>" + name + "</span></td>";
+	var button = "<td><button class='remove' type='button'></button></td>";
 	
 	var list_item = tag_li_start + table_start + checkbox 
 		+ text + button + table_end + tag_li_end;
@@ -28,20 +46,20 @@ function addListItem(name) {
 	// Add item to the list
 	$("#goods_list").append(list_item);
 	
-	// Remove button styles
-	$("button").button( {
+	// Remove button icon
+	$("button.remove").button( {
 		icons: {
 			primary: "ui-icon-closethick"
 		},
 		text: false
 	});
-	$("button").hide();
+	$("button.remove").hide();
 	// Remove button visible events
 	$("#" + list_id).mouseenter({id:list_id}, function(event_object) {
-		$("#" + event_object.data.id).find("button").show();
+		$("#" + event_object.data.id).find("button.remove").show();
 	});
 	$("#" + list_id).mouseleave({id:list_id}, function(event_object) {
-		$("#" + event_object.data.id).find("button").hide();
+		$("#" + event_object.data.id).find("button.remove").hide();
 	});
 	// Remove button click event
 	$("#" + list_id).find("button").click({id:list_id}, function(event_object) {
@@ -49,23 +67,38 @@ function addListItem(name) {
 	});
 	
 	// Edit item event
-	$("#" + list_id).dblclick({id:list_id}, function(event_object) {
-		var value = $("#" + event_object.data.id).find("#text").text();
+	$("#" + list_id).find("#text_cell").dblclick({id:list_id}, function(event_object) {
+		var value = $("#" + event_object.data.id).find(".text").text();
 		var text_box = "<input id='change' value='" + value + "'/>";
-		$("#" + event_object.data.id).find("#text").replaceWith("<td width='230' id='text'>" + text_box + "</td>");
+		
+		$("#" + event_object.data.id).find(".text")
+		.replaceWith("<td width='230' class='text'>" + text_box + "</td>");
 		
 		// Keys events
 		$("#" + event_object.data.id).find("input").keydown({li_id:event_object.data.id, old_text:value}, function(event_object) {
 			// Enter key (save text)
 			if(event_object.which == 13) {
 				var new_text = $("#" + event_object.data.li_id).find("#change").val();
-				$("#" + event_object.data.li_id).find("#text").replaceWith("<td width='230' id='text'>" + new_text + "</td>");
+				
+				$("#" + event_object.data.li_id).find(".text")
+				.replaceWith("<td width='230' class='text'>" + new_text + "</td>");
 			}
 			// Esc key (cancel saving)
 			else if(event_object.which == 27) {
-				$("#" + event_object.data.li_id).find("#text").replaceWith("<td width='230' id='text'>" + event_object.data.old_text + "</td>");
+				$("#" + event_object.data.li_id).find(".text")
+				.replaceWith("<td width='230' class='text'>" + event_object.data.old_text + "</td>");
 			}
 		});
+	});
+	
+	// Check box event
+	$("#" + list_id).find("input:checkbox").change({id:list_id}, function(event_object) {
+		if($(this).is(":checked")) {
+			$("#" + event_object.data.id).find(".text").attr("style", "text-decoration:line-through; opacity:0.5;");
+		}
+		else {
+			$("#" + event_object.data.id).find(".text").attr("style", "text-decoration:none;");
+		}
 	});
 	
 	list_id++;
